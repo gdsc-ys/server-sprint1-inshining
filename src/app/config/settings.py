675839d9dbb0b0106577d76de8f44bd418bfd5fa
inshining <inshining.kim@gmail.com>
2022-10-04 00:50:56 +0900

    😺[CHORE] : cursor 발급

--- src/app/config/settings.py
+++ src/app/config/settings.py
@@ -5,12 +5,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_session():
+def get_cursor():
     try:
         conn = psycopg2.connect(
             f'postgres://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}?sslmode=require'
         )
+        yield conn
     except Exception as e:
         print(e)
-    else:
-        return conn
+    finally:
+        cur.close()
+        conn.close()
