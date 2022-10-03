24d921e320a7cb384579943412f10808c2a698d4
inshining <inshining.kim@gmail.com>
2022-10-03 17:13:04 +0900

    😺[CHORE] : db 연결 테스트

--- src/app/api/boards.py
+++ src/app/api/boards.py
@@ -1,8 +1,18 @@
-from fastapi import APIRouter, Depends, HTTPException
+import os
 
+from fastapi import APIRouter, Depends, HTTPException
+import psycopg2
+
+from dotenv import load_dotenv
+load_dotenv()
 
 router = APIRouter()
 
 @router.get("/")
-def get_boards():
+async def get_boards():
+    with psycopg2.connect(f'postgres://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}?sslmode=require') as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM board')
+            print(cur.fetchall())
+
     return "Hello"
