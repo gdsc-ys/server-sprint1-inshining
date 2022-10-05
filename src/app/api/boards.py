99e070d5671197e8fa9e457e930fb82bea484416
inshining <inshining.kim@gmail.com>
2022-10-06 01:08:24 +0900

    🍀[FEAT] : 게시글 생성, 수정, 삭제 구현

--- src/app/api/boards.py
+++ src/app/api/boards.py
@@ -1,25 +1,21 @@
 import os
 
-from fastapi import APIRouter, Depends
-
-from config.settings import get_cursor
-from services.board import board_service
-from psycopg2._psycopg import cursor
-
-from db.sql import select_many_sql, select_one_sql
+from fastapi import APIRouter
+from db.sql import select_many_sql, select_one_sql, write_sql
+from schemas.board import BoardCreate, BoardUpdate
 
 router = APIRouter()
 
 @router.get("/")
 def get_boards():
     result = select_many_sql("SELECT * FROM board")
-
     return result
 
 # 게시물 생성 api
 @router.post("/new")
-def create_board():
-    pass
+def create_board(board : BoardCreate):
+    write_sql("INSERT INTO board(title, content) VALUES (%s, %s)", (board.title, board.content))
+    return board
 
 # 게시물 하나 조회
 @router.get("/{board_id}")
@@ -29,11 +25,14 @@ def get_board(board_id):
 
 # 게시물 하나 업데이트
 @router.put("/board/{board_id}")
-def update_board():
-    pass
+def update_board(board_id, board: BoardUpdate):
+    write_sql(f"UPDATE board SET title = %s , content = %s WHERE id = %s", (board.title, board.content, board_id))
+    return board
+
 
 # 게시물 하나 삭제
 @router.delete("/board/{board_id}")
-def delete_board():
-    pass
+def delete_board(board_id):
+    write_sql("DELETE FROM board WHERE id = %s", (board_id))
+    return {"msg" : "success"}
 
--- src/app/db/sql.py
+++ src/app/db/sql.py
@@ -1,11 +1,14 @@
 import os
 import psycopg2
+from dotenv import load_dotenv
+load_dotenv()
+
 DB_INFO = f'postgres://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}?sslmode=require'
 
-def write_sql(sql):
+def write_sql(sql, value):
     with psycopg2.connect(DB_INFO) as conn:
         with conn.cursor() as cursor:
-            cursor.execute(sql)
+            cursor.execute(sql, value)
         conn.commit()
 
 def select_one_sql(sql):
