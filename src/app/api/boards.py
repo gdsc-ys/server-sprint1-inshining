e18547f4099c6344044935b0c4d2324f4b395a59
inshining <inshining.kim@gmail.com>
2022-10-05 23:23:43 +0900

    ➕[ADD] : 필요한 api 엔드포인트 생성

--- src/app/api/boards.py
+++ src/app/api/boards.py
@@ -14,3 +14,24 @@ def get_boards(cursor : cursor = Depends(get_cursor)):
     result = board_service.get_boards(cursor)
 
     return result
+
+# 게시물 생성 api
+@router.post("/new")
+def create_board():
+    pass
+
+# 게시물 하나 조회
+@router.get("/board")
+def get_board():
+    pass
+
+# 게시물 하나 업데이트
+@router.put("/board/{board_id}")
+def update_board():
+    pass
+
+# 게시물 하나 삭제
+@router.delete("/board/{board_id}")
+def delete_board():
+    pass
+
