97e4b68d1558f6347788cb1e876b12ee99590d68
inshining <inshining.kim@gmail.com>
2022-10-06 22:41:49 +0900

    🍀[FEAT] : comment endpoints 추가

--- src/app/api/boards.py
+++ src/app/api/boards.py
@@ -3,6 +3,7 @@ import os
 from fastapi import APIRouter
 from db.sql import select_many_sql, select_one_sql, write_sql
 from schemas.board import BoardCreate, BoardUpdate
+from schemas.comment import CommentCreate, CommentUpdate
 
 router = APIRouter()
 
@@ -24,15 +25,35 @@ def get_board(board_id):
     return result
 
 # 게시물 하나 업데이트
-@router.put("/board/{board_id}")
+@router.put("/{board_id}")
 def update_board(board_id, board: BoardUpdate):
     write_sql(f"UPDATE board SET title = %s , content = %s WHERE id = %s", (board.title, board.content, board_id))
     return board
 
 
 # 게시물 하나 삭제
-@router.delete("/board/{board_id}")
+@router.delete("/{board_id}")
 def delete_board(board_id):
     write_sql("DELETE FROM board WHERE id = %s", (board_id))
     return {"msg" : "success"}
 
+@router.get("/comment/{comment_id}")
+def get_comment(comment_id):
+    result = select_one_sql(f"SELECT comment.id, comment.content, board.title, board.content  FROM comment LEFT OUTER JOIN board ON comment.board_id = board.id WHERE comment.id = {comment_id}")
+    return result
+
+
+@router.post("/{board_id}/comment")
+def create_comment(board_id, comment: CommentCreate):
+    write_sql("INSERT INTO comment(content, board_id) VALUES (%s, %s)", (comment.content, board_id))
+    return comment
+
+@router.put("/comment/{comment_id}")
+def update_comment(comment_id, comment: CommentUpdate):
+    write_sql(f"UPDATE comment SET content = %s WHERE id = %s", (comment.content, comment_id))
+    return comment
+
+@router.delete("/comment/{comment_id}")
+def delete_comment(comment_id):
+    write_sql("DELETE FROM comment WHERE id = %s", (comment_id))
+    return {"msg": "success"}
