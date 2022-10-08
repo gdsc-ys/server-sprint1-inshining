f9ea880aa45e068cd80c27ef656b399297696831
inshining <inshining.kim@gmail.com>
2022-10-09 02:57:38 +0900

    🍀[FEAT] : access_token 발급

--- src/app/api/login.py
+++ src/app/api/login.py
@@ -1,12 +1,26 @@
-from fastapi import APIRouter, Depends
+from datetime import timedelta
 
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+
+from config import security
 from config.settings import get_cursor
+from db.sql import select_one_sql
+from schemas.token import Token
 from services.board import board_service
 from psycopg2._psycopg import cursor
 
+from services.user import user_service
 
 router = APIRouter()
 
-@router.post("/")
-def login_user():
-    pass
+@router.post("/", response_model=Token)
+def login_user(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = user_service.authenticate(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=400)
+    access_token_expire = timedelta(minutes=11520)
+    return {
+        "access_token" : security.create_access_token(user[0], expire=access_token_expire),
+        "token_type": "bearer"
+    }
--- src/app/config/security.py
+++ src/app/config/security.py
@@ -1,4 +1,24 @@
+from datetime import timedelta, datetime
+from typing import Union, Any
+import secrets
+
+from jose import jwt
 from passlib.context import  CryptContext
+
 password_context = CryptContext(schemes=['bcrypt'])
 def convert_password_hash(password: str) -> str:
-    return password_context.has(password)
+    return password_context.hash(password)
+
+def create_access_token(subject : Union[str, Any], expire: timedelta = None):
+    if expire:
+        expire = datetime.utcnow() + expire
+    else:
+        expire = datetime.utcnow() + timedelta(
+            minutes= 11520
+        )
+    encode_data = {"expire": expire, "subject": str(subject)}
+    encoded_jwt  = jwt.encode(encode_data, secrets.token_urlsafe(32), algorithm="HS256")
+    return encoded_jwt
+
+def verify_password(plain_password:str, hashed_password: str) -> bool:
+    return password_context.verify(plain_password, hashed_password)
